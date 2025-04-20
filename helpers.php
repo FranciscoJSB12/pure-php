41ad1a78a3efa4669c2300380135b209072732ae
Francisco Salazar <francisco.salazar@itsense.com.co>
2025-04-20 08:52:48 -0400

    core: [add] functions to load views and partials

--- helpers.php
+++ helpers.php
@@ -11,3 +11,41 @@ function basePath($path = '')
 {
     return __DIR__ . '/' . $path;
 }
+
+/**
+ * @param string $name
+ * @return void
+ */
+
+function loadView($name)
+{
+    $path = basePath("views/{$name}.view.php");
+
+    checkFileBeforePrint($path, "View");
+}
+
+/**
+ * @param string $name
+ * @return void
+ */
+
+function loadPartial($name)
+{
+    $path = basePath("views/partials/{$name}.php");
+
+    checkFileBeforePrint($path, "Partial");
+}
+
+/**
+ * @param string $filePath
+ * @param string $fileType
+ * @return void
+ */
+function checkFileBeforePrint($filePath, $fileType)
+{
+    if (file_exists($filePath)) {
+        require $filePath;
+    } else {
+        echo "{$fileType} not found";
+    }
+}
